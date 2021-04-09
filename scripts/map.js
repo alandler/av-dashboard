5b3c0a60231d3e1302cf9143eddbb73e68af77b4
alandler <alandler@mit.edu>
2021-04-09 16:25:29 -0400

    Traffic markers, link to meta controller

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,13 @@
 var mymap = L.map('mapid').setView([42.3601, -71.0942], 17);
 
+markers = [[42.357413, -71.092688],
+            [42.357120, -71.092549],
+            [42.360140, -71.094898],
+            [42.360838, -71.096035],
+            [42.361734, -71.097559],
+            [42.362641, -71.098887]
+]
+
 mapbox_token = "pk.eyJ1IjoiYWxhbmRsZXIiLCJhIjoiY2tidjU1a3duMDJwajJ5bnhyYnZoMWgwdCJ9.wf5Fpih6PEVLN-LW_uAY_A"
 z = "512"
 y = "1"
@@ -13,3 +21,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: mapbox_token
 }).addTo(mymap);
+
+for (let position of markers){
+    console.log("Position: " + position)
+    var marker = L.marker([position[0], position[1]]).addTo(mymap).on('click', function(e) {
+        console.log(e.latlng)
+        window.location = "index.html";
+        // window.open("index.html");
+    });;
+    
+}
